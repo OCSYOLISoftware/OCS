41b7892e966e21f543428d73bd65dfb97c669bd2
OCSYOLISoftware <antonio.cerezo@outpatientcallsolutions.com>
2024-01-02 13:17:52 -0800

    Implementacióm de base de datos

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
+from fastapi import Depends,FastAPI, Body, Path, Query, Request
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, List
+from jwt_manager import create_token, validate_token
+from fastapi.security import HTTPBearer
 
 app = FastAPI()
 app.title = "Base de Datos Trabajadores"
@@ -9,10 +11,21 @@ app.version = "0.0.3"
 
 @app.get('/')
 
+class JWTBearer(HTTPBearer):
+    async def __call__(self, request: Request):
+        auth =  await super().__call__(request)
+        data = validate_token(auth.credentials)
+        if data['email'] != 'admin@admin':
+            raise HTTPException(status_code=403, details="Credenciales son invalidas")
+
+class User(BaseModel):
+    email: str
+    password: str
+
 class Employee(BaseModel):
     id: int
     employee_id: int = Field(ge=1000)
-    name: str = Field(min_length=3, max_length= 25)
+    name: str = Field(min_length=3, max_length= 50)
     dob: str  = Field(min_length=3, max_length= 25)
     date_of_admission: str  = Field(min_length=3, max_length= 25) 
     department: str  = Field(min_length=3, max_length= 25)
@@ -103,33 +116,42 @@ employees = [
 ]
 
 #Rutas
-#mostrar productos
-@app.get('/employees', tags=['employees'])
-def get_employee():
-    return employees
+@app.get('/', tags=['home'])
 
-#mostrar producto por ID
-@app.get('/employees/{employee_id}', tags=['employees'])
-def get_employee(employee_id: int):
+@app.post('/login', tags=['auth'])
+def login(user: User):
+    if user.email == 'admin@admin' and user.password == 'admin':
+        token: str = create_token(user.dict())
+    return JSONResponse(status_code=200, content=token)
+
+#mostrar empleado
+@app.get('/employees', tags=['employees'], response_model=List[Employee], status_code=200, dependencies=[Depends(JWTBearer())])
+def get_employee() -> List[Employee]:
+    return JSONResponse(status_code=200, content=employees)
+
+#mostrar empleado por ID
+@app.get('/employees/{employee_id}', tags=['employees'], response_model=Employee)
+def get_employee(employee_id: int = Path(ge=1000)) -> Employee:
     for employee in employees:
         if employee ["employee_id"] == employee_id:
-            return employee
-    return []
+            return JSONResponse(content=employee)
+    return JSONResponse(status_code=404, content=[])
 
-#filtrar por departamento y campaña
-@app.get("/employees/", tags=['employees'])
-def get_employess_by_department(department: str):
-    return [employee for employee in employees if employee['department'] == department]
+#filtrar por departamento
+@app.get("/employees/", tags=['employees'], response_model=List[Employee])
+def get_employess_by_department(department: str = Query(min_length=5, max_length=25)) -> List[Employee]:
+    data = [employee for employee in employees if employee['department'] == department]
+    return JSONResponse(content=data) 
 
 #Agrega una nueva lista
-@app.post('/employees', tags=['employees'])
-def create_employee(employee: Employee):
+@app.post('/employees', tags=['employees'], response_model=dict, status_code=201)
+def create_employee(employee: Employee) -> dict:
     employees.append(employee)
-    return employees
+    return JSONResponsec(status_code=201, content={"message": "Se ha registrado correctamente al trabajador."})
 
 #Modificar empleado
-@app.put('/employees/{employee_id}', tags=['employees'])
-def update_employee(employee_id: int, employee: Employee):
+@app.put('/employees/{employee_id}', tags=['employees'], response_model=dict, status_code=200)
+def update_employee(employee_id: int, employee: Employee) -> dict:
     for item in employees:
         if item['employee_id'] == employee_id:
             item['name'] = employee.name
@@ -140,12 +162,12 @@ def update_employee(employee_id: int, employee: Employee):
             item['position'] = employee.position
             item['supervisor'] = employee.supervisor
             item['salary'] = employee.salary
-            return employees
+            return JSONResponse(status_code=200, content={"message": "Se ha modificado correctamente al trabajador."})
 
 #Eliminar empleado
-@app.delete('/employees/{employee_id}', tags=['employees'])
-def delete_movie(employee_id: int):
+@app.delete('/employees/{employee_id}', tags=['employees'], response_model=dict, status_code=200)
+def delete_movie(employee_id: int) -> dict:
     for employee in employees:
         if employee['employee_id'] == employee_id:
             employees.remove(employee)
-            return employees
+            return JSONResponse(status_code=200, content={"message": "Se ha eliminado correctamente al trabajador."})
